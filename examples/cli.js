0314a3f6e4670a00e79d72cdb00df772bf7f0f8a
Eric Satterwhite <esatterwhite@wi.rr.com>
2017-12-18 07:23:30 -0600

    command: expose a propmt function a debug helper
    prompt function is basically passthrough the inquirer#prompt.
    command has a debug helper bound to the command instance for easier
    debug output

--- examples/cli.js
+++ examples/cli.js
@@ -1,47 +1,53 @@
 /*jshint laxcomma: true, smarttabs: true, node:true, mocha: true*/
 var cli = require("../")
 var path = require('path')
+
+cli.set('exitOnError', true);
+cli.set('color','green');
+cli.set('name', 'example');
+
 var Test = new cli.Command({
   description:"displays a simple hello world command"
-  ,usage:[
-    `${cli.bold("Usage:")} cli hello --interactive`,
-    `${cli.bold("Usage:")} cli hello --name=john`,
-    `${cli.bold("Usage:")} cli hello --name=john --name=marry --name=paul -v screaming`
+, name: 'hello'
+, usage:[
+    `${cli.bold("Usage:")} cli hello --interactive`
+  , `${cli.bold("Usage:")} cli hello --name=john`
+  , `${cli.bold("Usage:")} cli hello --name=john --name=marry --name=paul -v screaming`
   ]
 
-  ,flags:{
+, flags:{
 
     name:{
       type:[ String, Array ]
-      ,shorthand:'n'
-      ,description:"The name of the person to say hello to"
-      ,required:true
+    , shorthand:'n'
+    , description:"The name of the person to say hello to"
+    , required:true
     }
 
-    ,excited: {
+  , excited: {
       type:Boolean
-      ,shorthand: 'e'
-      ,description:"Say hello in a very excited manner"
-      ,default:false
+    , shorthand: 'e'
+    , description:"Say hello in a very excited manner"
+    , default:false
     }
 
-    ,volume:{
+  , volume:{
       type:String
-      ,choices:['normal', 'screaming']
-      ,description:"Will yell at each person"
-      ,default:'normal'
-      ,shorthand:'v'
+    , choices:['normal', 'screaming']
+    , description:"Will yell at each person"
+    , default:'normal'
+    , shorthand:'v'
     }
 
-    ,password: {
-      type:String,
-      mask:true,
-      description:"unique password",
-      shorthand:'p',
-      required: false
+  , password: {
+      type:String
+    , mask:true
+    , description:"unique password"
+    , shorthand:'p'
+    , required: false
     }
   }
-  ,run: async function( cmd, data ){
+, run: async function( cmd, data ){
     var out = [];
     var names = Array.isArray( data.name ) ? data.name : [ data.name ]
     for( var x =0; x< names.length; x++ ){
@@ -62,7 +68,6 @@ var Test = new cli.Command({
     return data.volume == 'screaming' ? out.toUpperCase() : out;
   }
 });
-cli.set('exitOnError', true)
-cli.use('hello', Test)
-cli.set('color','green');
+
+cli.use('hello', Test);
 cli.run();
