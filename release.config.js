74bd48d062bd7dcdee2408ca1f975f31ff493e00
Eric Satterwhite <esatterwhite@wi.rr.com>
2021-10-31 15:07:10 -0500

    fix(release): update release to regenerate docs.
    Addes back the exec stage to run the command that generates gh-pages
    with the rendered output from vuepress

--- release.config.js
+++ release.config.js
@@ -1,6 +1,29 @@
 'use strict'
 
+const config = require('@codedependant/release-config-npm')
+
 module.exports = {
   'extends': '@codedependant/release-config-npm'
 , 'branches': ['main']
+, 'plugins': remap(config.plugins)
+}
+
+function remap(plugins) {
+  const remapped = []
+
+  for (const [name, config] of plugins) {
+    if (name === '@semantic-release/git') {
+      remapped.push([
+        '@semantic-release/exec', {
+          prepareCmd: 'npm run docs:build'
+        }
+      ])
+      config.assets = [...config.assets, 'docs']
+    }
+
+    remapped.push([name, config])
+  }
+
+  return remapped
+
 }
