387aefd32afe884e63e1b166571da5dfe87759b4
Eric Satterwhite <esatterwhite@wi.rr.com>
2026-04-01 16:00:18 -0500

    chore(deps): codedependant/release-config-core@1.1.1
    updates the release config for changes and bugfixes with recent versions
    of semantic-release

--- release.config.js
+++ release.config.js
@@ -1,31 +1,6 @@
 'use strict'
 
-const config = require('@codedependant/release-config-npm')
-
 module.exports = {
-  'extends': '@codedependant/release-config-npm'
+  'extends': '@codedependant/release-config-core'
 , 'branches': ['main']
-, 'plugins': remap(config.plugins)
-}
-
-function remap(plugins) {
-  const remapped = []
-
-  for (const [name, config] of plugins) {
-    if (name === '@semantic-release/git') {
-      /*
-      remapped.push([
-        '@semantic-release/exec', {
-          prepareCmd: 'npm run docs:build'
-        }
-      ])
-      config.assets = [...config.assets, 'docs']
-      */
-    }
-
-    remapped.push([name, config])
-  }
-
-  return remapped
-
 }
